47484edcf9db796cf11a2524a82892ee62a63e77
Dhyey Vachhani <dhyey890@gmail.com>
2020-01-25 11:40:30 +0530

    Interview steps progress bar skeleton

---
+++ src/main/java/com/hireslate/controller/JobStagesController.java
@@ -0,0 +1,22 @@
+package com.hireslate.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hireslate.model.JobMasterEntity;
+
+@Controller
+@RequestMapping(value="admin/job-steps")
+public class JobStagesController{
+	
+	@RequestMapping(value="",method=RequestMethod.GET)
+	public String showJobStages(Model model) {
+		//List<JobMasterEntity> jobMaster = jobMasterService.viewJobMaster();
+		//model.addAttribute("jobMaster",jobMaster);
+		return "/job-stages/steps.jsp";
+	}
+}
